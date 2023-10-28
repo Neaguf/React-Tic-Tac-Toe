9cd13beae7bc5166dd8e38756e28e956c9cb3bea
Neagu Florin <112757160+Neaguf@users.noreply.github.com>
2023-10-28 18:16:55 +0300

    end

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   };
   let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
@@ -16,7 +24,6 @@ export default function Player({ initialName, symbol, isActive }) {
 
   function handleChange(event) {
     setPlayerName(event.target.value);
-    console.log(playerName);
   }
 
   return (
